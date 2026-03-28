6f3d3ab0af6f52cc63554a7eaa01ce946e18ec4f
samarreda7 <samarreda804@gmail.com>
2026-03-28 06:54:30 +0200

    Add DBContextFactory for AppDBContext to facilitate design-time database context creation

---
+++ ExamSystem.Data/DBContext/DBContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using ExamSystem.Data.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ExamSystem.Data.DBContext  
+{
+    public class DBContextFactory : IDesignTimeDbContextFactory<AppDBContext>
+    {
+        public AppDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDBContext>();
+            optionsBuilder.UseSqlServer(
+                "Data Source=SAMAR;Initial Catalog=ExamSystemDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;"
+            );
+            return new AppDBContext(optionsBuilder.Options);
+        }
+    }
+}
