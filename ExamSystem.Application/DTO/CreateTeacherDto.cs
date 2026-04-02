93f14cd428089732cace7bcda9954d45582081e9
samarreda7 <samarreda804@gmail.com>
2026-04-02 07:14:34 +0200

    feat: add CreateTeacherDto for teacher creation

---
+++ ExamSystem.Application/DTO/CreateTeacherDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTO
+{
+    public class CreateTeacherDto
+    {
+        public required String FirstName { get; set; }
+        public required String LastName { get; set; }
+        public required String Email { get; set; }
+        public required String Username { get; set; }
+        public required String Password { get; set; }
+        public required String PhoneNumber { get; set; }
+        public required Guid SubjectId { get; set; }
+    }
+}
