e2f7bfb57a1d20b54c5bd8a427acd48651fb4a12
samarreda7 <samarreda804@gmail.com>
2026-04-03 05:34:27 +0200

    feat: implement StudentGroupService to manage student assignments to groups

---
+++ ExamSystem.Application/IService/IStudentGroupService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.IService
+{
+    public interface IStudentGroupService
+    {
+        Task AssignStudentToGroupAsync(Guid studentId, Guid groupId, Guid teacherId);
+    }
+}
---
+++ ExamSystem.Application/Service/StudentGroupService.cs
@@ -0,0 +1,55 @@
+﻿using ExamSystem.Application.IService;
+using ExamSystem.Domain.IRepository;
+using ExamSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.Service
+{
+    public class StudentGroupService : IStudentGroupService
+    {
+        private readonly IUnitOfWork _unitofwork;
+        public StudentGroupService(IUnitOfWork unitofwork)
+        {
+            _unitofwork = unitofwork;
+        }
+        public async Task AssignStudentToGroupAsync(Guid studentId, Guid groupId, Guid teacherId)
+        {
+            var student = await _unitofwork.Students.GetByIdAsync(studentId);
+            if (student == null)
+            {
+                throw new KeyNotFoundException($"there is no student with this id {studentId}");
+            }
+            var group = await _unitofwork.Groups.GetByIdAsync(groupId);
+            if (group == null)
+            {
+                throw new KeyNotFoundException($"there is no group with this id {groupId}");
+            }
+            var teacher = await _unitofwork.Teachers.GetByIdAsync(teacherId);
+            if (teacher == null)
+            {
+                throw new KeyNotFoundException($"there is no teacher with this Id {teacherId}");
+            }
+            if (group.TeacherUserId != teacherId)
+            {
+                throw new UnauthorizedAccessException("This teacher is not assigned to the target group.");
+            }
+            bool Isduplicated = await _unitofwork.StudentGroup.IsStudentAssignedToThisGroupAsync(studentId, groupId);
+            if (Isduplicated)
+            {
+                throw new InvalidOperationException("this student is assigned to this group before");
+            }
+            var studentGroup = new StudentGroup
+            {
+                GroupId = groupId,
+                StudentId = studentId,
+            };
+            
+            await _unitofwork.StudentGroup.AddAsync(studentGroup);
+            await _unitofwork.SaveChangesAsync();
+        }
+    }
+}
