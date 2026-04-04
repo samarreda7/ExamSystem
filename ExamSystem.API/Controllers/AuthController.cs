50c3c96eb48be1f0e3128cbcef76ebd2491c6c7d
samarreda7 <samarreda804@gmail.com>
2026-04-04 08:12:47 +0200

    feat: add AuthController and StudentController with login and add student functionality

---
+++ ExamSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LogIn([FromBody]LoginDto user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+               var token = await _userService.LoginAsync(user);
+                return Ok(token);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExamSystem.API/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> AddStudent([FromBody]CreateStudentDto studentDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _studentService.AddStudentAsync(studentDto);
+                return Created(string.Empty, "Student added successfully");
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message); // 400 — null input
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(ex.Message);  // 409 — email/username already exists
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message); // 404 — role not found
+            }
+
+
+        }
+    }
+}
