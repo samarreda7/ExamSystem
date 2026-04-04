796ee4842aa391cac3fac7a3040b952169f9386d
samarreda7 <samarreda804@gmail.com>
2026-04-04 08:25:44 +0200

    feat: add TeacherController with functionality to add a teacher

---
+++ ExamSystem.API/Controllers/TeacherController.cs
@@ -0,0 +1,44 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherService _teacherService;
+        public TeacherController(ITeacherService teacherService)
+        {
+            _teacherService = teacherService;
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> AddTeacher([FromBody]CreateTeacherDto teacherDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _teacherService.AddTeacherAsync(teacherDto);
+                return CreatedAtAction(string.Empty, "Teacher added successfully");
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message); // 400 — null input
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(ex.Message);  // 409 — email/username already exists
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message); // 404 — role not found
+            }
+
+
+        }
+    }
+}
