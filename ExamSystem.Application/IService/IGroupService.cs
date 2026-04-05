a133e603f88499e2bb0ed4bb7e0dd27d2e0de4de
samarreda7 <samarreda804@gmail.com>
2026-04-05 11:14:28 +0200

    feat: implement GroupService with methods for adding groups and retrieving all groups

---
+++ ExamSystem.Application/IService/IGroupService.cs
@@ -0,0 +1,15 @@
+﻿using ExamSystem.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.IService
+{
+    public interface IGroupService
+    {
+        Task AddGroupAsync(CreateGroupDto groupDto);
+        Task<IEnumerable<ShowGroupDto>> GetAllGroupsAsync();
+    }
+}
---
+++ ExamSystem.Application/Service/GroupService.cs
@@ -0,0 +1,67 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using ExamSystem.Domain.IRepository;
+using ExamSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.Service
+{
+    public class GroupService : IGroupService
+    {
+        private readonly IUnitOfWork _unitofwork;
+        public GroupService(IUnitOfWork unitOfWork)
+        {
+            _unitofwork = unitOfWork;
+        }
+        public async Task AddGroupAsync(CreateGroupDto groupDto)
+        {
+            if (groupDto == null)
+            {
+                throw new ArgumentNullException(nameof(groupDto));
+            }
+            bool isGroupExist = await _unitofwork.Groups.IsGroupNameExistAsync(groupDto.Name);
+            if (isGroupExist)
+            {
+                throw new InvalidDataException("A Group with this name already exists.");
+            }
+            bool isTeacherExist = await _unitofwork.Teachers.IsTeacherExistAsync(groupDto.TeacherId);
+            if (!isTeacherExist)
+            {
+                throw new KeyNotFoundException($"No teacher found with Id: {groupDto.TeacherId}");
+            }
+
+            bool isSubjectExist = await _unitofwork.Subjects.IsSubjectExistAsync(groupDto.SubjectId);
+            if (!isSubjectExist)
+            {
+                throw new KeyNotFoundException($"No subject found with Id: {groupDto.SubjectId}");
+            }
+            var group = new Group
+            {
+                Name = groupDto.Name,
+                TeacherUserId=groupDto.TeacherId,
+                SubjectId= groupDto.SubjectId,
+            };
+
+            await _unitofwork.Groups.AddAsync(group);
+            await _unitofwork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ShowGroupDto>> GetAllGroupsAsync()
+        {
+            var subjects = await _unitofwork.Groups.GetAllGroupsAsync();
+            return subjects.Select(s => new ShowGroupDto
+            {
+                Id = s.Id,
+                Name = s.Name,
+                TeacherId=s.Teacher.UserId,
+                SubjectName=s.Subject.Name,       
+
+            });
+
+        }
+    }
+}
