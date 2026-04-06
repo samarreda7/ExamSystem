01e2bf5ff27365fe776ef90f21cd7e11df94c060
samarreda7 <samarreda804@gmail.com>
2026-04-06 06:10:22 +0200

    feat: add DTOs for assigning and reassigning students to groups

---
+++ ExamSystem.Application/DTO/AssignStudentToGroupDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTO
+{
+    public class AssignStudentToGroupDto
+    {
+        public Guid StudentId { get; set; }
+        public Guid GroupId { get; set; }
+
+    }
+}
---
+++ ExamSystem.Application/DTO/ReassignStudentToAnotherGroupDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTO
+{
+    public class ReassignStudentToAnotherGroupDto
+    {
+        public Guid StudentId { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid newGroupId { get; set; }
+
+
+    }
+}
