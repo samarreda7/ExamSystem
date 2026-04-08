5711ae886453eee17bc10ee156dc325115a723a3
samarreda7 <samarreda804@gmail.com>
2026-04-08 13:19:00 +0200

    feat: add ExamService and IExamService with AddExamAsync

---
+++ ExamSystem.Application/IService/IExamService.cs
@@ -0,0 +1,9 @@
+﻿using ExamSystem.Application.DTO;
+
+namespace ExamSystem.Application.IService
+{
+    public interface IExamService
+    {
+        Task AddExamAsync(Guid teacherId, CreateExamDto dto);
+    }
+}
---
+++ ExamSystem.Application/Service/ExamService.cs
@@ -0,0 +1,34 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using ExamSystem.Domain.IRepository;
+using ExamSystem.Domain.Models;
+
+
+namespace ExamSystem.Application.Service
+{
+    public class ExamService :IExamService
+    {
+        private readonly IUnitOfWork _unitofwork;
+        public ExamService(IUnitOfWork unitofwork)
+        {
+            _unitofwork = unitofwork;
+        }
+        public async Task AddExamAsync(Guid teacherId,CreateExamDto dto)
+        {
+            var teacher = await _unitofwork.Teachers.GetByIdAsync(teacherId);
+            if (teacher == null)
+            {
+                throw new KeyNotFoundException($"there is no teacher with this Id {teacherId}");
+            }
+            var exam = new Exam
+            {
+                Name = dto.Name,
+                TeacherUserId = teacher.UserId,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+            };
+            await _unitofwork.Exams.AddAsync(exam);
+            await _unitofwork.SaveChangesAsync();
+        }
+    }
+}
