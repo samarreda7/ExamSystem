b4f7979576671b032281224a3c94589518db7a06
samarreda7 <samarreda804@gmail.com>
2026-04-08 15:18:32 +0200

    feat: add ExamController with AddExamAsync endpoint and CreateExamDto

---
+++ ExamSystem.API/Controllers/ExamController.cs
@@ -0,0 +1,46 @@
+﻿using ExamSystem.Application.DTO;
+using ExamSystem.Application.IService;
+using ExamSystem.Domain.ValueTypes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        private readonly IExamService _examService;
+        public ExamController(IExamService examService)
+        {
+            _examService = examService;
+        }
+
+        [Authorize(Roles = nameof(RoleName.Teacher))]
+        [HttpPost("add")]
+        public async Task<IActionResult> AddExamAsync([FromBody]CreateExamDto dto) 
+        {
+            if (!ModelState.IsValid) 
+            {
+            return BadRequest(ModelState);
+            }
+            var teacherIdClaim = User.FindFirst("uid")?.Value;
+            if (string.IsNullOrEmpty(teacherIdClaim) || !Guid.TryParse(teacherIdClaim, out Guid teacherId))
+            {
+                return Unauthorized("Invalid token claims.");
+            }
+            try
+            {
+                await _examService.AddExamAsync(teacherId, dto);
+                return Ok("Exam Added Successfuly");
+            }
+            catch (KeyNotFoundException ex) 
+            {
+                return NotFound(new { error = ex.Message });
+            }
+
+
+        }
+
+    }
+}
