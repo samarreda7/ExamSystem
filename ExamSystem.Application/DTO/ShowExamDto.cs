7bad5b33633d9196d965fab5cb32d443b9f0958f
samarreda7 <samarreda804@gmail.com>
2026-04-09 05:10:19 +0200

    feat: add ShowExamDto class for exam data transfer

---
+++ ExamSystem.Application/DTO/ShowExamDto.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ExamSystem.Application.DTO
+{
+    public class ShowExamDto
+    {
+        public Guid Id {  get; set; }
+        public string Name { get; set; }
+        public string TeacherFirstName { get; set; }
+        public string TeacherLastName { get; set; }
+        public int QuestionsCount { get; set;}
+        public int GroupsCount { get; set; }
+
+    }
+}
