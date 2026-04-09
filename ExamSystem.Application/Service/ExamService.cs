9db3ac4079725692e914b45d289ba17e0abf43f8
samarreda7 <samarreda804@gmail.com>
2026-04-09 05:11:17 +0200

    feat: implement GetTeacherExamsAsync method to retrieve exams for a specific teacher

--- ExamSystem.Application/Service/ExamService.cs
+++ ExamSystem.Application/Service/ExamService.cs
@@ -30,5 +30,23 @@ namespace ExamSystem.Application.Service
             await _unitofwork.Exams.AddAsync(exam);
             await _unitofwork.SaveChangesAsync();
         }
+        public async Task<IEnumerable<ShowExamDto>> GetTeacherExamsAsync(Guid teacherId)
+        {
+            var teacher = await _unitofwork.Teachers.GetByIdAsync(teacherId);
+            if (teacher == null)
+            {
+                throw new KeyNotFoundException($"there is no teacher with this Id {teacherId}");
+            }
+            var exams = await _unitofwork.Exams.GetAllTeacherExamAsync(teacherId);
+            return exams.Select(exam => new ShowExamDto
+            {
+                Id = exam.Id,
+                Name = exam.Name,
+                TeacherFirstName = exam.Teacher.User.FirstName,
+                TeacherLastName = exam.Teacher.User.LastName,
+                QuestionsCount = exam.ExamQuestions.Count,
+                GroupsCount = exam.ExamGroups.Count,
+            });
+        }  
     }
 }
