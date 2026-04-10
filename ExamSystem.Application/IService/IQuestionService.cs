1f89f0f5c1d27c1a52acaeeded7a26e3813227bd
samarreda7 <samarreda804@gmail.com>
2026-04-10 03:56:00 +0200

    feat: add IQuestionService interface and QuestionService implementation stub

---
+++ ExamSystem.Application/IService/IQuestionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.IService
+{
+    internal interface IQuestionService
+    {
+    }
+}
