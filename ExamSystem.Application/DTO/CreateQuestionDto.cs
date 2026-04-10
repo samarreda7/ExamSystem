d5b3a72acf7129bd2d3bc360e89de19e6972da46
samarreda7 <samarreda804@gmail.com>
2026-04-10 04:36:43 +0200

    feat: add CreateQuestionDto for handling new question creation requests

---
+++ ExamSystem.Application/DTO/CreateQuestionDto.cs
@@ -0,0 +1,18 @@
+﻿using ExamSystem.Domain.Models;
+using ExamSystem.Domain.ValueTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTO
+{
+    public class CreateQuestionDto
+    {
+        public string Text { get; set; }
+        public QuestionType Type { get; set; }
+        public Guid SubjectId { get; set; }
+
+    }
+}
