3546b3f6257316c4a33f6ea6b64d9278d963fdc5
samarreda7 <samarreda804@gmail.com>
2026-04-11 09:27:36 +0200

    feat: Add ShowQuestionDto to map question and teacher details

---
+++ ExamSystem.Application/DTO/ShowQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using ExamSystem.Domain.Models;
+using ExamSystem.Domain.ValueTypes;
+
+namespace ExamSystem.Application.DTO
+{
+    public class ShowQuestionDto
+    {
+        public Guid Id { get; set; }
+        public required string Text { get; set; }
+        public QuestionType Type { get; set; }
+        public required string TeacherFirstName { get; set; }
+        public required string TeacherLastName { get; set; }
+
+    }
+}
