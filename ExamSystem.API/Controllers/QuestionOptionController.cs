787c0db21da9dfc7ee0ad122147d4e7b60171577
samarreda7 <samarreda804@gmail.com>
2026-04-13 03:05:08 +0200

    feat(api): add GET endpoint in QuestionOptionController to fetch option details

--- ExamSystem.API/Controllers/QuestionOptionController.cs
+++ ExamSystem.API/Controllers/QuestionOptionController.cs
@@ -60,6 +60,31 @@ namespace ExamSystem.API.Controllers
         }
 
         [Authorize(Roles = nameof(RoleName.Teacher))]
+        [HttpGet("{optionId:guid}")]
+        public async Task<IActionResult> GetOptionByIdAsync([FromRoute] Guid optionId)
+        {
+            var teacherIdClaim = User.FindFirst("uid")?.Value;
+            if (string.IsNullOrEmpty(teacherIdClaim) || !Guid.TryParse(teacherIdClaim, out Guid teacherId))
+            {
+                return Unauthorized("Invalid token claims.");
+            }
+
+            try
+            {
+                var option = await _questionOptionService.GetOptionByIdAsync(optionId, teacherId);
+                return Ok(option);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { error = ex.Message });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return StatusCode(403, new { error = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = nameof(RoleName.Teacher))]
         [HttpPut("{optionId:guid}")]
         public async Task<IActionResult> UpdateOptionAsync([FromRoute] Guid optionId, [FromBody] UpdateQuestionOptionDto dto)
         {
