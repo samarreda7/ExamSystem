7f778872e12a411ed8ea2307058c32d8602be53f
samarreda7 <samarreda804@gmail.com>
2026-04-14 16:41:24 +0200

    feat(api): add endpoint to get questions by exam id

--- ExamSystem.API/Controllers/ExamQuestionController.cs
+++ ExamSystem.API/Controllers/ExamQuestionController.cs
@@ -83,5 +83,30 @@ namespace ExamSystem.API.Controllers
                 return Conflict(new { error = ex.Message });
             }
         }
+
+        [Authorize(Roles = nameof(RoleName.Teacher))]
+        [HttpGet("{examId:guid}/questions")]
+        public async Task<IActionResult> GetQuestionsByExamIdAsync([FromRoute] Guid examId)
+        {
+            var teacherIdClaim = User.FindFirst("uid")?.Value;
+            if (string.IsNullOrEmpty(teacherIdClaim) || !Guid.TryParse(teacherIdClaim, out Guid teacherId))
+            {
+                return Unauthorized("Invalid token claims.");
+            }
+
+            try
+            {
+                var questions = await _examQuestionService.GetQuestionsByExamIdAsync(teacherId, examId);
+                return Ok(questions);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { error = ex.Message });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return StatusCode(403, new { error = ex.Message });
+            }
+        }
     }
 }
