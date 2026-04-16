bc33de3b1d2fa5e507374578530fdb0d19495663
samarreda7 <samarreda804@gmail.com>
2026-04-16 13:14:17 +0200

    feat(dto): add InitAdminDto mapping for first-time admin registration

---
+++ ExamSystem.Application/DTO/InitAdminDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTO
+{
+    public class InitAdminDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
