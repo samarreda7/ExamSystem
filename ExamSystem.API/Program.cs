3fbbd21646aec441acce45d15762e2037174cf05
samarreda7 <samarreda804@gmail.com>
2026-04-17 15:22:27 +0200

    feat(api): register student exam answer and result services

--- ExamSystem.API/Program.cs
+++ ExamSystem.API/Program.cs
@@ -67,6 +67,8 @@ builder.Services.AddScoped<IExamGroupService, ExamGroupService>();
 builder.Services.AddScoped<IExamQuestionService, ExamQuestionService>();
 builder.Services.AddScoped<IQuestionService, QuestionService>();
 builder.Services.AddScoped<IQuestionOptionService, QuestionOptionService>();
+builder.Services.AddScoped<IStudentExamAnswerService, StudentExamAnswerService>();
+builder.Services.AddScoped<IStudentExamResultService, StudentExamResultService>();
 
 
 
