26aa5d74272bcbbaae327f488a449bcbcd6b5e7e
samarreda7 <samarreda804@gmail.com>
2026-04-20 11:11:17 +0200

    feat(application): add current user response dto

---
+++ ExamSystem.Application/DTO/ShowCurrentUserDto.cs
@@ -0,0 +1,13 @@
+namespace ExamSystem.Application.DTO
+{
+    public class ShowCurrentUserDto
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+    }
+}
